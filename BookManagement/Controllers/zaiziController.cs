0f25967364e4a8bc03d47304fbf7c93a9e269dbf
Fuxiaodong-alone <2774753923@qq.com>
2021-07-24 00:08:04 +0800

    崽子2

---
+++ BookManagement/Controllers/zaiziController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookManagement.Controllers
+{
+    public class zaiziController : Controller
+    {
+        // GET: zaizi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
