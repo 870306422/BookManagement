df40fd43906900bb0eb93831b624830e03aca856
DZ <2980737615qq.com>
2021-07-25 00:18:11 +0800

    显示Totallist数据表的信息

--- BookManagementTwo/App_Start/RouteConfig.cs
+++ BookManagementTwo/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BookManagementTwo
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Totallist", action = "Show", id = UrlParameter.Optional }
             );
         }
     }
